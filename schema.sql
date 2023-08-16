6b2df6e54c5dbdd94d86375efa1779cf087e66f1
Abdelaziz Mahdi <abdelaziz.dv@gmail.com>
2023-08-16 16:37:18 +0300

    Add a column species of type string  schema.sql

--- schema.sql
+++ schema.sql
@@ -1,11 +1,13 @@
-/* Database schema to keep the structure of entire database. */
+/* Database schema to keep the structure of the entire database. */
 
 CREATE TABLE animals (
     ID SMALLSERIAL NOT NULL,
-    NAME VARCHAR(100) NOT NULL,
+    NAME VARCHAR(50) NOT NULL,
     date_of_birth DATE,
     escape_attempts SMALLINT,
     neutered BOOLEAN,
     weight_kg REAL,
     PRIMARY KEY(id)
 );
+
+ALTER TABLE animals ADD species VARCHAR(100);
