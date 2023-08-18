625cf02060ec6e3ad60857fe708b75eb0dd7dc01
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-08-18 16:34:56 +0300

    Update animals table species_id and owners_id

--- data.sql
+++ data.sql
@@ -57,3 +57,40 @@ INSERT INTO species (
 )
 VALUES ('Pokemon'), 
 ('Digimon');
+
+-- Modify your inserted animals so it includes the species_id value:If the name ends in "mon" it will be Digimon All other animals are Pokemon
+
+UPDATE animals
+SET species_id = 1
+WHERE name NOT LIKE '%mon';
+
+UPDATE animals
+SET species_id = 2
+WHERE name LIKE '%mon';
+
+-- Modify your inserted animals to include owner information (owner_id):
+-- Sam Smith owns Agumon.
+-- Jennifer Orwell owns Gabumon and Pikachu.
+-- Bob owns Devimon and Plantmon.
+-- Melody Pond owns Charmander, Squirtle, and Blossom.
+-- Dean Winchester owns Angemon and Boarmon.
+
+UPDATE animals
+SET owner_id = 1
+WHERE name = 'Agumon';
+
+UPDATE animals
+SET owner_id = 2
+WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animals
+SET owner_id = 3
+WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals
+SET owner_id = 4
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+SET owner_id = 5
+WHERE name IN ('Angemon', 'Boarmon');
